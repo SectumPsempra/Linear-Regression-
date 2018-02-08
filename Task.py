72a78985937b0c897627f64415e273f0ebace8ba
Half_Blood_Prince <bhattamit991@gmail.com>
2018-02-08 16:28:46 +0530

    Updated Task file

--- Task.py
+++ Task.py
@@ -1,58 +1,59 @@
-import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
-
-#  generate data
-
-
-STATES = SCHOOL_LOCATION = MOTHER_TONGUE = GENDER = PARENTS_INCOME_PER_ANNUM = PERCENT_IN_12TH_CLASS \
-    = AVG_HRS_STUDIED = np.array(0)
-
-PROBABILITY_IIT = 0
-JEE_SCORE = 0
-
-STATES = ["Andhra Pradesh","Arunachal Pradesh ","Assam","Bihar","Chhattisgarh", "Delhi", "Goa","Gujarat","Haryana","Himachal Pradesh","Jammu and Kashmir","Jharkhand","Karnataka","Kerala","Madhya Pradesh","Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Odisha","Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura","Uttar Pradesh","Uttarakhand","West Bengal"]
-#print(len(STATES))
-
-# choose gender randomly over 100 values.
-GENDER = np.random.randint(2, size=1000)
-
-# choose MOTHER_TONGUE randomly from a distribution of any languages
-lang = ['Marathi', 'Hindi', 'Kannada', 'Tamilian', 'Gujarati']
-MOTHER_TONGUE = np.random.randint(5, size=1000)
-MOTHER_TONGUE = [lang[MOTHER_TONGUE[x]] for x in MOTHER_TONGUE]
-#print(MOTHER_TONGUE)
-
-# Take Parents income around three range of values -
-# 0 - Less than or equal to 1 LPA
-# 1 - Less than or equal to 5 LPA but greater than 1 LPA
-# 2 - greater than 5 LPA
-PARENTS_INCOME_PER_ANNUM = np.random.randint(3, size=1000)
-#print(PARENTS_INCOME_PER_ANNUM[:50])
-
-# Distribute marks between 33 and 100
-PERCENT_IN_12TH_CLASS = np.random.randint(33, 100, size=1000)
-#print(PERCENT_IN_12TH_CLASS)
+import pandas as pd
 
 
-AVG_HRS_STUDIED = np.random.choice(range(4, 15), size=1000)
-#print(AVG_HRS_STUDIED[AVG_HRS_STUDIED > 10])
+LANGUAGE = ['Marathi', 'Hindi', 'Kannada', 'Tamilian', 'Gujarati']
 
-# Now, JEE_SCORE is a function of AVG_HRS_STUDIED and PERCENT_IN_12TH_CLASS
-JEE_SCORE = ((AVG_HRS_STUDIED * PERCENT_IN_12TH_CLASS) / (14.0*99.0)) * 100.0
-JEE_SCORE *= 3.6
-#print(JEE_SCORE[:10])
+STATES_NAME = ["Andhra Pradesh","Arunachal Pradesh ","Assam","Bihar","Chhattisgarh", "Delhi", "Goa","Gujarat","Haryana","Himachal Pradesh","Jammu and Kashmir","Jharkhand","Karnataka","Kerala","Madhya Pradesh","Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Odisha","Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura","Uttar Pradesh","Uttarakhand","West Bengal"]
 
-data = ["GENDER", "MOTHER_TONGUE", "PARENTS_INCOME_PER_ANNUM", "PERCENT_IN_12TH_CLASS", "AVG_HRS_STUDIED", "JEE_SCORE"]
-#print(data)
-
-col = np.array(zip(GENDER, MOTHER_TONGUE, PARENTS_INCOME_PER_ANNUM, PERCENT_IN_12TH_CLASS, AVG_HRS_STUDIED, JEE_SCORE))
-#print(col[:10])
+COLUMN_NAME = ["GENDER", "STATES", "MOTHER_TONGUE", "PARENTS_INCOME_PER_ANNUM", "PERCENT_IN_12TH_CLASS", "AVG_HRS_STUDIED", "JEE_SCORE"]
 
 
-df = pd.DataFrame(col.reshape(-1, 6), columns=data)
-#print(df[:10])
+class CreateDataSet:
 
+    def __init__(self, file_name):
+
+        self.file_name = file_name
+
+        self.STATES = np.random.randint(len(STATES_NAME), size=1000)
+        self.MOTHER_TONGUE = np.random.randint(len(LANGUAGE), size=1000)
+        self.GENDER = np.random.randint(2, size=1000)
+
+        # Take Parents income around three range of values -
+        # 0 - Less than or equal to 1 LPA
+        # 1 - Less than or equal to 5 LPA but greater than 1 LPA
+        # 2 - greater than 5 LPA
+        self.PARENTS_INCOME_PER_ANNUM = np.random.randint(3, size=1000)
+
+        self.PERCENT_IN_12TH_CLASS = np.random.randint(33, 100, size=1000)
+        self.AVG_HRS_STUDIED = np.random.choice(range(4, 15), size=1000)
+        self.JEE_SCORE = np.zeros(1000)
+
+    def define_parameters(self):
+
+        self.STATES = [STATES_NAME[stateName] for stateName in self.STATES]
+        self.MOTHER_TONGUE = [LANGUAGE[lang] for lang in self.MOTHER_TONGUE]
+
+        self.JEE_SCORE = ((self.AVG_HRS_STUDIED * self.PERCENT_IN_12TH_CLASS) / (14.0 * 99.0)) * 100.0
+        self.JEE_SCORE *= 3.6  # Convert to (0-360) range.
+
+    def generate_dataset(self):
+        data = np.array(
+            zip(self.GENDER, self.STATES, self.MOTHER_TONGUE, self.PARENTS_INCOME_PER_ANNUM, self.PERCENT_IN_12TH_CLASS, self.AVG_HRS_STUDIED, self.JEE_SCORE))
+
+        dataframe = pd.DataFrame(data.reshape(-1, 7), columns=COLUMN_NAME)
+        # Only uncomment to generate dataset
+        dataframe.to_csv(self.file_name, sep=',')
+        print "File generation successful, file %s is generated in your local directory" % self.file_name
+
+
+fileName = raw_input("Enter the name of the file with extension(.csv or any other): ")
+initialize = CreateDataSet(fileName)
+initialize.define_parameters()
+initialize.generate_dataset()
+
+"""
+import matplotlib.pyplot as plt
 
 # Plot the JEE_SCORE vs PERCENT_IN_12TH_CLASS
 #------------------------------------------------------------------------------------------
@@ -64,9 +65,4 @@ df = pd.DataFrame(col.reshape(-1, 6), columns=data)
 # plt.show()
 #-------------------------------------------------------------------------------------------
 
-
-# Only uncomment to save dataFrame - df to csv file
-file_name = "data_IIT.csv"
-df.to_csv(file_name, sep=',')
-
-
+"""
